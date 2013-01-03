e6773a5594b94d53e439f040a632ab71ed09aeec
Raymond Luk <raymond.h.luk@gmail.com>
2013-01-02 22:28:58 -0800

    initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstagramPops::Application.config.secret_token = 'ad0b19ce26b66a9ad43f1fcb40b5de0f0e4bc60dd35ad17c813e45545915b69dda2763a3b9e55edd9125ef267e1a6eb551290dffff2d6282f42811427bacf93b'
